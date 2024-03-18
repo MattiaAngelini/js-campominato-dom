fb7a7c40efff7c29aee84c1107839503d5b557b8
MattiaAngelini <mattiaangelini1993@gmail.com>
2024-03-18 14:36:39 +0100

    Fixed: style of numbers of cells

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,39 @@
+//Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: 
+//le bombe. Attenzione: nella stessa cella può essere posizionata al massimo una bomba,
+// perciò nell’array delle bombe non potranno esserci due numeri uguali.
 
+
+//In seguito l'utente clicca su una cella: se il numero è presente nella lista dei numeri generati 
+// abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina.
+// Altrimenti la cella cliccata si colora di azzurro
+// e l'utente può continuare a cliccare sulle altre celle.
+
+
+//La partita termina quando il giocatore clicca su una bomba o quando raggiunge il numero massimo possibile di numeri consentiti (ovvero quando ha rivelato tutte le celle che non sono bombe).
+//Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba.
 //START
 
-// 1) - CREARE UN BOTTONE CHE GENERERA' LA GRIGLIA:
-//      creo un evento click in cui rimuovo la classe d-none.
-
-//creo selettore per la griglia
+//selettore della griglia
 const gridGame = document.querySelector("#grid-game");
 
-//creo selettore per il tasto play 
+//selettore per il tasto play 
 const buttonPlay = document.querySelector("#button-play");
 
-//creo evento al play che fa comparire la griglia
+const buttonSquare = document.querySelector(".square");
+console.log(buttonSquare)
+
+//evento al play che fa comparire la griglia
 buttonPlay.addEventListener ('click', function (){ 
  
-    //RESET dopo nuovo click
+    //RESET dopo ogni nuovo click
     gridGame.innerHTML = "";
 //creo selettore difficoltà selezionata.
 const choice = document.querySelector("#levels").value;
     
-    
     let numbersOfSquare;
     let numbersOfCells;
-// Creazione numero dei quadrati totali e numero quadrati per riga, in base scelta selezionata.
+// Condizione per la creazione numero dei quadrati in base al valore dell'input 
+// selezionato dall'utente.
 if (choice === 'easy') {
     numbersOfSquare = 100;
     numbersOfCells = 10;
@@ -33,31 +45,29 @@ if (choice === 'easy') {
     numbersOfCells = 7;
 }
 
-// la griglia compare al click, ora mi servono i quadrati.
-// ho a disposizione il numero degli elementi al click in base alla difficoltà.
-// devo creare una funzione che crea un numero di div pari a numbersOfsquare.
-
-  // Ciclo for che genera un div tante volte quanto il numero degli squares.
-  // il div ha evento click per fare cambiare colore allo square.
+  // Ciclo che genera un certo numero di div in base alla scelta dell'utente (condizione in alto).
   for (let i = 1; i <= numbersOfSquare; i++) {
-    const newSquare = createDiv(i);
-    newSquare.addEventListener('click', function (){    
-    newSquare.classList.add('styles-selection');
-    })
+    const newSquare = createDiv(i, numbersOfCells);
     gridGame.append(newSquare); 
+    newSquare.addEventListener('click', function (){    
+        newSquare.classList.add('styles-selection');
+        })
 }
 
 });
 
 
 // FUNCTIONS
-// Funzione che genera un div
-   function createDiv (number) {
-   let newDiv = document.createElement('div');
-   newDiv.classList.add('square');
-   newDiv.innerHTML = number; // Aggiungo il numero al contenuto del div
-   return newDiv; 
-}
+// Funzione che genera un div con classe square di dimensioni proporzionate al numero delle celle.
+function createDiv(number, numbersOfCells) {
+    let newDiv = document.createElement('div');
+    newDiv.classList.add('square');
+    newDiv.innerHTML = number; // Aggiungo il numero al contenuto del div
+    newDiv.style.width = `calc(100% / ${numbersOfCells})`;
+    newDiv.style.height = `calc(100% / ${numbersOfCells})`;
+    return newDiv;
+  }
+  
 
 
 
