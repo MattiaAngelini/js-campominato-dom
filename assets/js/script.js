949978fbc16ead3234d029dfbc5de4db1e554b70
MattiaAngelini <mattiaangelini1993@gmail.com>
2024-03-18 15:40:16 +0100

    Add: function random numbers

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,15 +2,6 @@
 //le bombe. Attenzione: nella stessa cella può essere posizionata al massimo una bomba,
 // perciò nell’array delle bombe non potranno esserci due numeri uguali.
 
-
-//In seguito l'utente clicca su una cella: se il numero è presente nella lista dei numeri generati 
-// abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina.
-// Altrimenti la cella cliccata si colora di azzurro
-// e l'utente può continuare a cliccare sulle altre celle.
-
-
-//La partita termina quando il giocatore clicca su una bomba o quando raggiunge il numero massimo possibile di numeri consentiti (ovvero quando ha rivelato tutte le celle che non sono bombe).
-//Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba.
 //START
 
 //selettore della griglia
@@ -22,16 +13,17 @@ const buttonPlay = document.querySelector("#button-play");
 const buttonSquare = document.querySelector(".square");
 console.log(buttonSquare)
 
+let numbersOfSquare;
+let numbersOfCells;
 //evento al play che fa comparire la griglia
 buttonPlay.addEventListener ('click', function (){ 
  
     //RESET dopo ogni nuovo click
     gridGame.innerHTML = "";
+
 //creo selettore difficoltà selezionata.
 const choice = document.querySelector("#levels").value;
     
-    let numbersOfSquare;
-    let numbersOfCells;
 // Condizione per la creazione numero dei quadrati in base al valore dell'input 
 // selezionato dall'utente.
 if (choice === 'easy') {
@@ -45,17 +37,19 @@ if (choice === 'easy') {
     numbersOfCells = 7;
 }
 
+let bombs = generateNonRepeatedNumbers(numbersOfSquare);
+console.log(bombs)
+
   // Ciclo che genera un certo numero di div in base alla scelta dell'utente (condizione in alto).
   for (let i = 1; i <= numbersOfSquare; i++) {
     const newSquare = createDiv(i, numbersOfCells);
     gridGame.append(newSquare); 
     newSquare.addEventListener('click', function (){    
-        newSquare.classList.add('styles-selection');
+    newSquare.classList.add('styles-selection');
         })
 }
 
 });
-
 
 // FUNCTIONS
 // Funzione che genera un div con classe square di dimensioni proporzionate al numero delle celle.
@@ -68,6 +62,19 @@ function createDiv(number, numbersOfCells) {
     return newDiv;
   }
   
+  //funzione che genera un numer random da 1 a max.
+  function generateNonRepeatedNumbers(max) {
+    if (max < 16) {
+        return [];
+    }
+    let numbers = [];
+    let randomNumber;
 
-
-
+    while (numbers.length < 16) {
+        randomNumber = Math.floor(Math.random() * max) + 1;
+        if (!numbers.includes(randomNumber)) {
+            numbers.push(randomNumber);
+        }
+    }
+    return numbers;
+}
